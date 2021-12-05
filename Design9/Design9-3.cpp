af139ce15604f5c694a95005a566c5efee9297d6
Bayathy <misosoup0321@icloud.com>
2021-12-05 21:54:07 +0900

    update

---
+++ Design9/Design9-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+namespace exam {
+ namespace japanese {
+ void question() {cout << "‚ ‚È‚½‚Í“ú–{Œê‚ªD‚«‚Å‚·‚©?" << endl;}
+ int score;
+ }
+ namespace english {
+ void question() {cout << "Do you like English?" << endl;}
+ int score;
+ }
+}
+int main() {
+ exam::japanese::score = 90;
+ exam::english::score = 10;
+ exam::japanese::question();
+ {
+ using namespace exam;
+ cout << japanese::score << endl;
+ }
+ {
+ using namespace exam::english;
+ question();
+ cout << score << endl;
+ }
+}
